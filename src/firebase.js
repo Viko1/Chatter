94927b5d26da644d394b7dc7aa09bf6bf2ceac27
Viko1 <vitaliiropiak@gmail.com>
2020-08-31 23:36:27 +0200

    chat

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDlO7lvFJTdkBz7kQvH1QF7MKN1QQmDEc8",
+  authDomain: "whats-app-clone-66cfa.firebaseapp.com",
+  databaseURL: "https://whats-app-clone-66cfa.firebaseio.com",
+  projectId: "whats-app-clone-66cfa",
+  storageBucket: "whats-app-clone-66cfa.appspot.com",
+  messagingSenderId: "596988004970",
+  appId: "1:596988004970:web:a617d4b6ffd9a6ff7d9813",
+  measurementId: "G-L3P9951EQ8"
+};
