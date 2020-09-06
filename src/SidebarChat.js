9b1985a4fa0ff84fd5f551a49d3f66f8f1300adf
Viko1 <vitaliiropiak@gmail.com>
2020-09-06 23:24:35 +0200

    router and db

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react'
 import './SidebarChat.css'
 import {Avatar} from "@material-ui/core";
+import db from "./firebase"
 
-function SidebarChat({addNewChat}) {
+function SidebarChat({id, name, addNewChat}) {
    const [seed, setSeed] = useState("");
 
    useEffect(()=> {
@@ -14,6 +15,10 @@ function SidebarChat({addNewChat}) {
 
       if (roomName){
          // do some clever database stuff...
+         db.collection('rooms').add({
+            name: roomName,
+
+         })
       }
    };
 
@@ -21,7 +26,7 @@ function SidebarChat({addNewChat}) {
       <div className="sidebarChat">
          <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
          <div className="sidebarChat__info">
-            <h2>Room name</h2>
+            <h2>{name}</h2>
             <p>Last message...</p>
          </div>
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase"
+
 const firebaseConfig = {
   apiKey: "AIzaSyDlO7lvFJTdkBz7kQvH1QF7MKN1QQmDEc8",
   authDomain: "whats-app-clone-66cfa.firebaseapp.com",
@@ -8,3 +10,13 @@ const firebaseConfig = {
   appId: "1:596988004970:web:a617d4b6ffd9a6ff7d9813",
   measurementId: "G-L3P9951EQ8"
 };
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth, provider}
+export default db;
