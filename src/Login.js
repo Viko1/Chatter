e9c627ef0cd7c4daeb84a4781e58148bf8a89c50
Viko1 <vitaliiropiak@gmail.com>
2020-09-09 23:36:15 +0200

    Google auth

--- src/Login.js
+++ src/Login.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './Login.css'
 import {Button} from "@material-ui/core";
+import {auth, provider} from "./firebase";
+import {useStateValue} from "./StateProvider";
+import {actionTypes} from "./reducer";
 
 function Login() {
+   const [{}, dispatch] = useStateValue();
 
-   const signIn = () => {};
+   const signIn = () => {
+      auth
+         .signInWithPopup(provider)
+         .then(result => {
+            dispatch({
+               type: actionTypes.SET_USER,
+               user: result.user,
+            })
+         } )
+         .catch((error) => alert(error.message));
+   };
 
    return (
       <div className="login">
          <div className="login__container">
          <img src='https://ci4.googleusercontent.com/proxy/5eZblmxZruz3cIHXOfuvvspnYQ74BcverXUC21LIUc4vo9mKi-fqqT6HBjLcdW8-UK_er3wHug9Tsj8y78HJtQ0Ui6djSpYiy2EgR-1dI8XlW2jIVOC3Y00Ghy_q9bRS6gbD33aQHVGJ8PL0D1oPzVgmr3CFoOgA=s0-d-e1-ft#https://www.freelogodesign.org/file/app/client/thumb/1a6542b2-9b32-478e-936a-ee18557e69b4_200x200.png' alt=''/>
             <div className="login__text">
-               <h1>Sign in to WhatsApp</h1>
+               <h1>Sign in to Chatter</h1>
             </div>
-
             <Button type="submit" onClick={signIn}>
                Sign In With Google
             </Button>
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,9 +7,11 @@ import MoreVertIcon from '@material-ui/icons/MoreVert'
 import SearchIcon from '@material-ui/icons/Search';
 import SidebarChat from './SidebarChat'
 import db from './firebase'
+import {useStateValue} from "./StateProvider";
 
 function Sidebar() {
    const [rooms, setRooms] = useState([]);
+   const [{user}, dispatch] = useStateValue();
 
    useEffect(() => {
       db.collection('rooms').onSnapshot(snapshot => {
@@ -27,7 +29,7 @@ function Sidebar() {
    return (
       <div className="sidebar">
          <div className="sidebar__header">
-            <Avatar/>
+            <Avatar src={user?.photoURL}/>
             <div className="sidebar__headerRight">
                <IconButton>
                   <DonutLargeIcon/>
